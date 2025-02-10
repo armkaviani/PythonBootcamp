3a4e7b908dd50dcb7eaf5d36676c8676ad8bd772
armakaviNI <armaghankaviani@yahoo.com>
2025-02-10 22:35:27 +0100

    The pong game is initialized and the first step for creating th both paddle has been implemented.
    Signed-off-by: armakaviNI <armaghankaviani@yahoo.com>

---
+++ PongGame_Turtle003/buildingPongGame.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+top_paddle = Paddle((100, 100))
+
+screen.listen()
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
