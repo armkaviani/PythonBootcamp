5d4e38eaac5b42b00f7ceaabc9e0918d3744077d
armakaviNI <armaghankaviani@yahoo.com>
2025-02-12 22:30:56 +0100

    The ball position  and scoreboard functions have been added.
    Signed-off-by: armakaviNI <armaghankaviani@yahoo.com>

--- PongGame_Turtle003/paddle.py
+++ PongGame_Turtle003/paddle.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
 
-class Paddle(Turtle):
 
+class Paddle(Turtle):
+    
     def __init__(self, position):
         super().__init__()
-        self.shape('square')
-        self.color('white')
+        self.shape("square")
+        self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
