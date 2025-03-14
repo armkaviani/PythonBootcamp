55ac6bb414c0a3a3a8f98cf6b6577e1d16a784fe
armakaviNI <armaghankaviani@yahoo.com>
2025-03-14 19:51:47 +0100

    A while loop has been written to keep guessing, and a guessed_states list has been defined to record the correct guesses.
    Signed-off-by: armakaviNI <armaghankaviani@yahoo.com>

--- GuessGame_panda/us_state_game.py
+++ GuessGame_panda/us_state_game.py
@@ -10,21 +10,29 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
-#Pop up box
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 
 data = pd.read_csv("50_states.csv")
 all_states= data.state.to_list()
+guessed_state =[]
 
-#Convert the guess to Title case
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    #with item(), accessing the single item contained in panda. Because with state_data.x, it tries to access the index and column.
-    t.goto(state_data.x.item(), state_data.y.item())
-    t.write(answer_state)
+while len(guessed_state) < 50:
+
+#Pop up box
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+    #Convert the guess to Title case
+    if answer_state in all_states:
+
+        #Add answer_state to the guessed_state
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+
+        #with item(), accessing the single item contained in panda. Because with state_data.x, it tries to access the index and column.
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
 
 
 screen.exitonclick()
