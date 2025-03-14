6d4d60f976b1bdcb17afa81faf34b49dad6aa1bf
armakaviNI <armaghankaviani@yahoo.com>
2025-03-14 19:31:15 +0100

    Implemented title case conversion for guessed state and Wrote correct gusses onto the map.
    Signed-off-by: armakaviNI <armaghankaviani@yahoo.com>

--- GuessGame_panda/us_state_game.py
+++ GuessGame_panda/us_state_game.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 #add the US map 
 screen = turtle.Screen()
@@ -9,6 +10,29 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
+#Pop up box
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+data = pd.read_csv("50_states.csv")
+all_states= data.state.to_list()
+
+#Convert the guess to Title case
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    #with item(), accessing the single item contained in panda. Because with state_data.x, it tries to access the index and column.
+    t.goto(state_data.x.item(), state_data.y.item())
+    t.write(answer_state)
+
+
+screen.exitonclick()
+
+
+
+
+
 
 #For fetching the coordinate the states
 # def get_mouse_click_coor(x, y):
@@ -17,5 +41,7 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_coor)
 
 #alternative way of keeping the screen open 
-turtle.mainloop()
+#turtle.mainloop()
+
+
 
