2c434576fc17243a88077aab2f80f5bfa6505d38
armakaviNI <armaghankaviani@yahoo.com>
2025-03-14 18:20:14 +0100

    A new game has been added for guessing the US states using the Turtle and Pandas libraries.
    Signed-off-by: armakaviNI <armaghankaviani@yahoo.com>

---
+++ GuessGame_panda/us_state_game.py
@@ -0,0 +1,16 @@
+import turtle
+
+#add the US map 
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+
+#load the map as a new shape
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+
+
+
+
+screen.exitonclick()
