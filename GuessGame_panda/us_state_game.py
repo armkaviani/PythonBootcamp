d1ffd4d1f58d3fe27044ecdd19fdfc66db78a2ce
armakaviNI <armaghankaviani@yahoo.com>
2025-03-14 18:32:20 +0100

    Replaced turtle.mainloop() with screen.exitonclick().
    Signed-off-by: armakaviNI <armaghankaviani@yahoo.com>

--- GuessGame_panda/us_state_game.py
+++ GuessGame_panda/us_state_game.py
@@ -10,7 +10,12 @@ turtle.addshape(image)
 turtle.shape(image)
 
 
+#For fetching the coordinate the states
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
 
+# turtle.onscreenclick(get_mouse_click_coor)
 
+#alternative way of keeping the screen open 
+turtle.mainloop()
 
-screen.exitonclick()
