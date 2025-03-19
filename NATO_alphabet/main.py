2e23e9320fd528902ace4db7dca43bc5f05532c8
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-19 22:36:17 +0100

    User input for the name and conversion to NATO phonetic alphabet have been implemented.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- NATO_alphabet/main.py
+++ NATO_alphabet/main.py
@@ -2,3 +2,8 @@ from nato_phonetic import NatoAlphabet
 
 def main():
     nato = NatoAlphabet("nato_phonetic_alphabet.csv")
+
+    name = input("Enter a name: ")
+    nato_words = nato.get_phonetic_name(name) 
+
+    
