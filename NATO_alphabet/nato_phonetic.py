222335d679fcdd0695d136ac1d5a08dfbe351dfe
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-19 22:34:47 +0100

    The get_phonetic_name method to convert name to NATO phonetic alphabet has been added.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- NATO_alphabet/nato_phonetic.py
+++ NATO_alphabet/nato_phonetic.py
@@ -6,3 +6,6 @@ class NatoAlphabet:
         self.nato_alphabet = pd.read_csv(csv_file)
         self.nato_alphabet_dict = {row.letter: row.code for (index, row) in self.nato_alphabet.iterrows()}
 
+    def get_phonetic_name(self, name):
+        phonetic_name = [self.nato_alphabet_dict[letter] for letter in name.upper()]
+        return phonetic_name
