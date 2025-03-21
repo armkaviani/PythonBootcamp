183c9faf7cbaadff4147ea6fa650d1531cf4b718
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-21 23:47:31 +0100

    The MilesToKmConverter class with GUI window have been initialized.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ MileToKilometer_Converter_Tkinter/miles_to_km_converter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+class MilesToKmConverter:
+        
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Miles to Kilometer Converter")
+        self.window.config(padx=20, pady=20)
+
+   
