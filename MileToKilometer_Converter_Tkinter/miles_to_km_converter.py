d25a8e9f6aa587869cf5bb9607926b1415917160
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-22 00:09:48 +0100

    "Add 'is equal to' label."
    "Add 'is equal to' label in MilesToKmConverter."
    
    OA
    
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- MileToKilometer_Converter_Tkinter/miles_to_km_converter.py
+++ MileToKilometer_Converter_Tkinter/miles_to_km_converter.py
@@ -19,3 +19,7 @@ class MilesToKmConverter:
         self.miles_label = Label(text="Miles")
         self.miles_label.grid(column=2, row=0)
    
+         # Label: "is equal to"
+        self.is_equal_label = Label(text="is equal to")
+        self.is_equal_label.grid(column=0, row=1)
+
