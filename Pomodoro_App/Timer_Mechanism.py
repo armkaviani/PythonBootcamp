c851b74bd63f2aeee486c3355c8651646b03a56d
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-25 22:39:51 +0100

    The start_timer method to manage work and break sessions has been implemented.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Pomodoro_App/Timer_Mechanism.py
+++ Pomodoro_App/Timer_Mechanism.py
@@ -13,3 +13,20 @@ class TimerMechanism:
     def __init__(self, ui):
         self.ui = ui
         self.countdown = Countdown(ui)
+
+    
+    def start_timer(self):
+        self.countdown.reps += 1
+        work_sec= self.WORK_MIN * 60
+        short_break_sec= self.SHORT_BREAK_MIN * 60
+        long_break_sec = self.LONG_BREAK_MIN * 60
+
+        if self.countdown.reps % 8 == 0:
+            self.countdown.count_down(long_break_sec)
+            self.ui.update_title("Break", RED)
+        elif self.countdown.reps % 2 == 0:
+            self.countdown.count_down(short_break_sec)
+            self.ui.update_title("Break", PINK)
+        else:
+            self.countdown.count_down(work_sec)
+            self.ui.update_title("Work", GREEN)
