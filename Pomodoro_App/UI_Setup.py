f72aca6ce9242ed8a486d3e229f08f602f1c0733
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-25 22:59:21 +0100

    Integrate TimerMechanism and ResetMechanism with respective button commands.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Pomodoro_App/UI_Setup.py
+++ Pomodoro_App/UI_Setup.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from Timer_Mechanism import TimerMechanism
+from Reset_Mechanism import ResetMechanism
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,6 +14,9 @@ class UISetup:
         self.root.title("Pomodoro")
         self.root.config(padx=100, pady=50, bg=YELLOW, highlightthickness=0)
 
+        self.timer_mechanism = TimerMechanism(self)
+        self.reset_mechanism = ResetMechanism(self.timer_mechanism)
+
         self.canvas = Canvas(self.root, width=200, height=224, bg=YELLOW)
         self.tomato_img = PhotoImage(file="tomato.png")
         self.canvas.create_image(102, 112, image=self.tomato_img)
@@ -21,10 +26,10 @@ class UISetup:
         self.title_label = Label(self.root, text="Timer", bg=YELLOW, font=(FONT_NAME, 50, "bold"), fg=GREEN)
         self.title_label.grid(column=1, row=0) 
 
-        self.start_button = Button(self.root, text="Start", highlightthickness=0)
+        self.start_button = Button(self.root, text="Start", highlightthickness=0, command=self.timer_mechanism.start_timer)
         self.start_button.grid(column=0, row=2)  
 
-        self.reset_button = Button(self.root, text="Reset", highlightthickness=0)
+        self.reset_button = Button(self.root, text="Reset", highlightthickness=0, command=self.reset_mechanism.reset)
         self.reset_button.grid(column=2, row=2)      
 
         self.check_marks = Label(self.root, fg=GREEN, bg=YELLOW)
