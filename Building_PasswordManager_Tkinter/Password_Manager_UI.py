59365af60d69619877040a5432069763c6a0cd10
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-28 00:06:44 +0100

    Updated button command to call generate_password method from PasswordGenerator class.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Building_PasswordManager_Tkinter/Password_Manager_UI.py
+++ Building_PasswordManager_Tkinter/Password_Manager_UI.py
@@ -33,7 +33,7 @@ class PasswordManagerUI:
         self.password_saver = PasswordSaver(self.website_entry, self.email_entry, self.password_entry)
 
         # Buttons
-        self.generate_password_button = Button(text="Generate Password")
+        self.generate_password_button = Button(text="Generate Password", command=self.password_generator.generate_password(self.password_entry))
         self.generate_password_button.grid(row=3, column=2)
 
         self.add_button = Button(text="Add", width=36, command=self.password_saver.save)
