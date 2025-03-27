19a06ca9469a8db2d7ff20ac32b40795581cd507
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-27 23:46:19 +0100

    Created PasswordSaver class to handle saving passwords and managing form data fields.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Building_PasswordManager_Tkinter/Password_Saver.py
@@ -0,0 +1,5 @@
+class PasswordSaver:
+    def __init__(self, website_field, email_field, password_field):
+        self.website_field = website_field
+        self.email_field = email_field
+        self.password_field = password_field
