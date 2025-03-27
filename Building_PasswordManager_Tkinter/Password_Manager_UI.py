d60643d80f7d547654c6146a29149a04ed636d9e
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-27 23:18:04 +0100

    Initialize PasswordManagerUI class with basic window setup.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Building_PasswordManager_Tkinter/Password_Manager_UI.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+class PasswordManagerUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Password Manager")
+        self.window.config(padx=50, pady=50)
