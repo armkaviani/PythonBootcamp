faf0125662297f8878a73c8bc21f7a06d6ce9f0c
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-27 23:52:48 +0100

    Implemented password generation logic.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Building_PasswordManager_Tkinter/Password_Generator.py
+++ Building_PasswordManager_Tkinter/Password_Generator.py
@@ -1,5 +1,14 @@
+from random import choice, randint, shuffle
+
+
 class PasswordGenerator:
+
     def __init__(self):
         self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    def generate_password(self, password_field):
+        password_letters = [choice(self.letters) for _ in range(randint(8, 10))]
+        password_symbols = [choice(self.symbols) for _ in range(randint(2, 4))]
+        password_numbers = [choice(self.numbers) for _ in range(randint(2, 4))]
