4475512402092b8ed2544ad9dbafb6107406c25c
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-03-27 23:19:39 +0100

    Canvas and logo image setup in PasswordManagerUI have been added.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Building_PasswordManager_Tkinter/Password_Manager_UI.py
+++ Building_PasswordManager_Tkinter/Password_Manager_UI.py
@@ -5,3 +5,8 @@ class PasswordManagerUI:
         self.window = Tk()
         self.window.title("Password Manager")
         self.window.config(padx=50, pady=50)
+    
+        self.canvas = Canvas(height=200, width=200)
+        self.logo_img = PhotoImage(file="logo.png")
+        self.canvas.create_image(100, 100, image=self.logo_img)
+        self.canvas.grid(row=0, column=1)
