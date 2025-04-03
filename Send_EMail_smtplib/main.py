957cfbc004903ed976aec060b4b0e906fa782299
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-03 21:44:46 +0200

    Added main function to initialize and execute BirthdayEmailer.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Send_EMail_smtplib/main.py
+++ Send_EMail_smtplib/main.py
@@ -1,6 +1,10 @@
 from Birthday_Email import BirthdayEmailer
 
 def main():
+    my_email = "your_mail@gmail.com"
+    my_pass = "your_pass"
+    emailer = BirthdayEmailer(my_email, my_pass)
+    emailer.send_birthday_email()
 
 if __name__ == "__main__":
     main()
