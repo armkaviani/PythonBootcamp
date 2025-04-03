1e91bfbd96469849144ec2791f6f384fef12ac1d
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-03 22:00:23 +0200

    Initialize the UI components and connect with the game logic.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Flashcard_Project_Tkinter/FlashCard_UI.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+
+class FlashcardUI:
+    def __init__(self, frame, flashcard_game):
+        self.frame = frame
+        self.flashcard_game = flashcard_game
+        self.canvas = Canvas(width=800, height=526)
+        self.card_front_image = PhotoImage(file="images/card_front.png")
+        self.card_back_image = PhotoImage(file="images/card_back.png")
+        self.card_background = self.canvas.create_image(400, 263, image=self.card_front_image)
+        self.title_card = self.canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+        self.word_card = self.canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+        self.canvas.config(bg="#B1DDC6", highlightthickness=0)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+
+        self.right_button_img = PhotoImage(file="images/right.png")
+        self.check_button = Button(image=self.right_button_img, highlightthickness=0)
+        self.check_button.grid(row=1, column=1)
+
+        self.wrong_button_img = PhotoImage(file="images/wrong.png")
+        self.unknown_button = Button(image=self.wrong_button_img, highlightthickness=0)
+        self.unknown_button.grid(row=1, column=0)
