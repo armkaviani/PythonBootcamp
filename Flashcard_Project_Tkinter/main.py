b77f90cc0eed5e8e114fc6d760b58318f0dff90f
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-03 22:30:32 +0200

    Initialize the Tkinter app and start the flashcard game.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Flashcard_Project_Tkinter/main.py
+++ Flashcard_Project_Tkinter/main.py
@@ -1,4 +1,23 @@
+from tkinter import *
+from FlashCard_Data import FlashcardData
+from FlashCard_Game import FlashcardGame
+from FlashCard_UI import FlashcardUI
+
+
 def main():
+    frame = Tk()
+    frame.title("Flashy")
+    frame.config(padx=50, pady=50, bg="#B1DDC6")
+
+    flashcard_data = FlashcardData()  # Load and manage data
+    flashcard_ui = FlashcardUI(frame, None)  # Create the UI components
+    flashcard_game = FlashcardGame(flashcard_data, flashcard_ui)  # Create the game logic
+
+    flashcard_ui.flashcard_game = flashcard_game  # Link the UI to the game logic
+
+    flashcard_game.random_card()  # Start the game by showing the first card
+    frame.mainloop()  # Start the Tkinter event loop
+
 
 if __name__ == "__main__":
     main()
