f78ed15b80e928df42763a8ca6f66bf670c703e1
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-03 21:25:09 +0200

    Added load_letter template methods to read email from file and added load_birthdays method to read and process birthday data from CSV.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Send_EMail_smtplib/Birthday_Email.py
+++ Send_EMail_smtplib/Birthday_Email.py
@@ -12,3 +12,10 @@ class BirthdayEmailer:
         today = dt.datetime.now()
         return today.day, today.month
 
+    def load_birthdays(self, file_path="birthdays.csv"):
+        lines = pd.read_csv(file_path)
+        return {(row['day'], row['month']): row for (index, row) in lines.iterrows()}
+    
+    def load_letter_template(self, file_path="letter.txt"):
+        with open(file_path) as text:
+            return text.read()
