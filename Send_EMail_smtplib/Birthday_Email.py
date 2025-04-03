9c81f976a4a8badb2184ff15e3804179ce58c537
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-03 21:22:03 +0200

    Added load_birthdays method to read and process birthday data from CSV.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Send_EMail_smtplib/Birthday_Email.py
+++ Send_EMail_smtplib/Birthday_Email.py
@@ -12,31 +12,3 @@ class BirthdayEmailer:
         today = dt.datetime.now()
         return today.day, today.month
 
-    def load_birthdays(self, file_path="birthdays.csv"):
-        lines = pd.read_csv(file_path)
-        return {(row['day'], row['month']): row for (index, row) in lines.iterrows()}
-
-    def load_letter_template(self, file_path="letter.txt"):
-        with open(file_path) as text:
-            return text.read()
-
-    def send_email(self, recipient_email, subject, body):
-        with smtplib.SMTP(self.smtp_server) as connection:
-            connection.starttls()
-            connection.login(user=self.email, password=self.password)
-            connection.sendmail(from_addr=self.email,
-                                to_addrs=recipient_email,
-                                msg=f"Subject:{subject}\n\n{body}")
-
-    def send_birthday_email(self):
-        today_tuple = self.get_today_tuple()
-        birthdays_dict = self.load_birthdays()
-
-        if today_tuple in birthdays_dict:
-            birth_person = birthdays_dict[today_tuple]
-            letter_template = self.load_letter_template()
-            birth_text = letter_template.replace("[NAME]", birth_person["name"])
-            self.send_email(birth_person["email"], "Happy Birthday!", birth_text)
-            print(f"Sent birthday email to {birth_person['name']}")
-        else:
-            print("No birthdays today.")
