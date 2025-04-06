eee079a8c2db7bc6d58e3f14c01de509021e19a4
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-07 00:09:53 +0200

    The class question data has been added to get all data over API.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ GUI_Quiz_APP/data.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class QuestionData:
+
+    def __init__(self):
+        self.data_param = {"amount" : 10, "type" : "boolean"}
+
+
+    def question_data(self):
+        self.response = requests.get( "https://opentdb.com/api.php", params=self.data_param)
+        self.response.raise_for_status()
+        self.data = self.response.json()
+        return self.data["results"]
+    
+        
+ 
