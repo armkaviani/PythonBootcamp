d77c1cd76a09eb6069ca7e80c698890b98b4c46d
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-24 23:40:26 +0200

    The method to fetch top 3 news articles related to the company has been added. Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Stock_Trading_News_Alert/News_Sender.py
+++ Stock_Trading_News_Alert/News_Sender.py
@@ -5,3 +5,8 @@ class GetNews:
     NEWS_API_KEY = "e34f452b25d746db9ca9709a32c18ccc"
     NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
     COMPANY_NAME = "Tesla Inc"
+
+def fetch_articles(self):
+        params = {"apikey": self.NEWS_API_KEY, "qInTitle": self.COMPANY_NAME}
+        articles = requests.get(self.NEWS_ENDPOINT, params=params).json()["articles"]
+        return articles[:3]
