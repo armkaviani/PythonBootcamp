14dc24f7c35df3462e80c0c9f31398346521953c
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-24 23:43:17 +0200

    Add constructor to initialize Twilio client and store phone numbers.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Stock_Trading_News_Alert/News_Sender.py
+++ Stock_Trading_News_Alert/News_Sender.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 
 class GetNews:
@@ -6,7 +7,17 @@ class GetNews:
     NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
     COMPANY_NAME = "Tesla Inc"
 
+
+def __init__(self, twilio_sid, twilio_auth, from_number, to_number):
+        self.client = Client(twilio_sid, twilio_auth)
+        self.from_number = from_number
+        self.to_number = to_number
+
+
 def fetch_articles(self):
         params = {"apikey": self.NEWS_API_KEY, "qInTitle": self.COMPANY_NAME}
         articles = requests.get(self.NEWS_ENDPOINT, params=params).json()["articles"]
         return articles[:3]
+
+
+
