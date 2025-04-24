497bf3616dcd839ae1a334d683ddfb99c8a55ba5
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-24 23:29:51 +0200

    Add constructor to set stock API request parameters.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Stock_Trading_News_Alert/Stock_Price.py
+++ Stock_Trading_News_Alert/Stock_Price.py
@@ -6,4 +6,8 @@ class StockPrice:
     STOCK_ENDPOINT = "https://www.alphavantage.co/query"
     STOCK_API_KEY = "BIRIG5133W1XXB2T"
 
+    def __init__(self):
+        self.stock_params = {"function": "TIME_SERIES_DAILY", "symbol": self.STOCK_NAME, "apikey": self.STOCK_API_KEY,}
+
+
 
