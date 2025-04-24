21a111df5b3c48eefda4aadabbae3bc1d1ac3103
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-24 23:50:16 +0200

    Add logic to send news articles via Twilio if stock price change exceeds threshold.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Stock_Trading_News_Alert/main.py
+++ Stock_Trading_News_Alert/main.py
@@ -11,7 +11,20 @@ VERIFIED_NUMBER = "your_verified_phone_number"
 
 def main():
 
+    stock = StockPrice()
+    should_get_news, diff_percent, up_down = stock.check_price_difference()
+
+    if should_get_news:
+        news = GetNews(
+            twilio_sid=TWILIO_SID,
+            twilio_auth=TWILIO_AUTH_TOKEN,
+            from_number=VIRTUAL_TWILIO_NUMBER,
+            to_number=VERIFIED_NUMBER
+        )
+        news.send_articles(stock_symbol=StockPrice.STOCK_NAME, up_down=up_down, diff_percent=diff_percent)
+    else:
+        print("Stock price movement not significant enough. No news sent.")
+
+
 if __name__ == "__main__":
-
-
     main()
