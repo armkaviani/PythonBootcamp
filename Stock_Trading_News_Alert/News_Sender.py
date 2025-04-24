1c9360aac44fbd70679ecfe67930f1e767cf6100
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-24 23:44:38 +0200

    Add method to send top 3 articles as SMS using Twilio.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Stock_Trading_News_Alert/News_Sender.py
+++ Stock_Trading_News_Alert/News_Sender.py
@@ -8,16 +8,26 @@ class GetNews:
     COMPANY_NAME = "Tesla Inc"
 
 
-def __init__(self, twilio_sid, twilio_auth, from_number, to_number):
-        self.client = Client(twilio_sid, twilio_auth)
-        self.from_number = from_number
-        self.to_number = to_number
+    def __init__(self, twilio_sid, twilio_auth, from_number, to_number):
+            self.client = Client(twilio_sid, twilio_auth)
+            self.from_number = from_number
+            self.to_number = to_number
 
 
-def fetch_articles(self):
-        params = {"apikey": self.NEWS_API_KEY, "qInTitle": self.COMPANY_NAME}
-        articles = requests.get(self.NEWS_ENDPOINT, params=params).json()["articles"]
-        return articles[:3]
+    def fetch_articles(self):
+            params = {"apikey": self.NEWS_API_KEY, "qInTitle": self.COMPANY_NAME}
+            articles = requests.get(self.NEWS_ENDPOINT, params=params).json()["articles"]
+            return articles[:3]
 
+
+    def send_articles(self, stock_symbol, up_down, diff_percent):
+        articles = self.fetch_articles()
+        messages = [
+            f"{stock_symbol}: {up_down}{diff_percent}%\nHeadline: {a['title']}\nBrief: {a['description']}"
+            for a in articles
+        ]
+
+        for msg in messages:
+            self.client.messages.create(body=msg, from_=self.from_number, to=self.to_number)
 
 
