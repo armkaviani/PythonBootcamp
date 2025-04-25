89346b1ed4061ba5288bff2002f1658a766d3dfc
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-04-25 23:34:33 +0200

    Add CreateGraph class to define and create a new graph.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Habit_Tracker/config.py
+++ Habit_Tracker/config.py
@@ -3,4 +3,7 @@ PIXELA_API = "https://pixe.la/v1/users"
 TOKEN = "your_token"
 USERNAME = "your_username"
 
+GRAPH_API = f"{PIXELA_API}/{USERNAME}/graphs"
+GRAPH_ID = "graph-1"
+
 
--- Habit_Tracker/main.py
+++ Habit_Tracker/main.py
@@ -1,9 +1,14 @@
 from Create_Username import CreateUsername
+from Create_Graph import CreateGraph
 
 
 def main():
     username = CreateUsername()
     username.create_username()
+
+    user_graph = CreateGraph()
+    user_graph.create_graph()
+
 
 
 if __name__ == "__main__":
