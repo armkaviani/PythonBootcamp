05f06660d22189f59224e790bfbf13ab619244f3
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-05-01 21:53:52 +0200

    In main methos check if data_sheet contains any values for rthe "iataCode" key. If not, then the IATA Codes column is empty in the Google sheet.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Flight_Deal_Finder/main.py
+++ Flight_Deal_Finder/main.py
@@ -1,8 +1,18 @@
 from Data_Manager import DataManager
+from Flight_Search import FlightSearch
 
 def main():
     data_manager = DataManager()
-    data_manager.get_data()
+    data_sheet = data_manager.get_data()
+
+    flight_search = FlightSearch()
+
+    if data_sheet[0]["iataCode"] == "":
+        for value in data_sheet:
+            value["iataCode"] = flight_search.get_destination(value["city"])
+        print(f"data_sheet:\n {data_sheet}")
+   
+
 
 if __name__ == "__main__":
     main()
