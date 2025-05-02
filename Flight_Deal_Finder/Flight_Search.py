838dd08c062a46e2322331a5cf02bb8cd9406ac3
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-05-02 22:39:49 +0200

    A __init__(self)  in the FlightSearch class has been created. Then three variables: an _api_key, an _api_secret, and a _token.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Flight_Deal_Finder/Flight_Search.py
+++ Flight_Deal_Finder/Flight_Search.py
@@ -1,5 +1,16 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        self._api_key = os.environ["AMADEUS_API_KEY"]
+        self._api_secret = os.environ["AMADEUS_API_SECRET"]
+        self._token = self._get_new_token()
+
     def get_destination(self, city_name):
         code = "TESTING"
         return code
