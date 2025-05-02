96910f0d4b8446fd7c85fd8b90c5bf027625384c
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-05-02 23:55:11 +0200

    Retrieves the IATA code for a specified city.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Flight_Deal_Finder/Flight_Search.py
+++ Flight_Deal_Finder/Flight_Search.py
@@ -4,6 +4,8 @@ import os
 
 load_dotenv()
 TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations"
+
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
@@ -30,5 +32,23 @@ class FlightSearch:
 
 
     def get_destination(self, city_name):
-        code = "TESTING"
+        print(f"Using this token to get destination {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "keyword": city_name,
+            "max": "1",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(url=IATA_ENDPOINT, headers=headers, params=query)
+        
+        print(f"Status code {response.status_code}. Airport IATA: {response.text}")
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city_name}.")
+            return "Not Found"
+
         return code
