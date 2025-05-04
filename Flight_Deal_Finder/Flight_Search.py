97dae9b43b452e5f3af2bcbff1930098955d14cf
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-05-04 23:35:45 +0200

    This function initially checks if the data contains valid flight entries. If no valid data is found,     it returns a FlightData object containing "N/A" for all fields. Otherwise, it starts by assuming the first     flight in the list is the cheapest. It then iterates through all available flights in the data, updating      the cheapest flight details whenever a lower-priced flight is encountered.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Flight_Deal_Finder/Flight_Search.py
+++ Flight_Deal_Finder/Flight_Search.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 
 class FlightSearch:
@@ -52,3 +54,35 @@ class FlightSearch:
             return "Not Found"
 
         return code
+    
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        # print(f"Using this token to check_flights() {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
