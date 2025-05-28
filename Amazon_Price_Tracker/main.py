bb35eb5c70670dccc980702456a3eff00ffa66d4
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-05-28 19:15:27 +0200

    Integrate EMailAlert to send email if price condition is met.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Amazon_Price_Tracker/main.py
+++ Amazon_Price_Tracker/main.py
@@ -1,10 +1,13 @@
 from ScrapePrice import ScrapePrice
-
+from EMail_Alert import EMailAlert
 
 def main():
 
     scrape_price = ScrapePrice()
     price_as_float, price_with_currency, title = scrape_price.get_price()
+
+    email_alert = EMailAlert(title)
+    email_alert.send_email(price_as_float, price_with_currency, scrape_price.url)
 
 
 if __name__ == "__main__":
