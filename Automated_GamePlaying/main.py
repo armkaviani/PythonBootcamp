331fa6ffb76cde07992856f288632b2d0f71d2c2
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-06-02 23:29:17 +0200

    Add 5-minute timer to stop bot and display cookies per second.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Automated_GamePlaying/main.py
+++ Automated_GamePlaying/main.py
@@ -12,7 +12,12 @@ def main():
             affordable = bot.get_affordable_upgrades(item_prices)
             bot.purchase_upgrade(affordable)
             bot.timeout = time.time() + 5
+            
 
+        if time.time() > bot.five_min:
+            cps = bot.driver.find_element(By.ID, "cps").text
+            print(f"Cookies per second: {cps}")
+            break
 
 if __name__ == "__main__":
     main()
