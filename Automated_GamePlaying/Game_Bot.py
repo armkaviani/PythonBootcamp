18a116cb7b0cb65cd589d5e307f82a4dabbf470d
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-06-02 22:39:32 +0200

    Locate main cookie and store items on page load.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Automated_GamePlaying/Game_Bot.py
+++ Automated_GamePlaying/Game_Bot.py
@@ -7,3 +7,7 @@ class CookieClickerBot:
         chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+        self.cookie = self.driver.find_element(By.ID, "cookie")
+        items = self.driver.find_elements(By.CSS_SELECTOR, "#store div")
+        self.item_ids = [item.get_attribute("id") for item in items]
