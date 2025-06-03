110534ad56677cf200b47265d31d0f0d2487f9a4
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-06-03 21:41:32 +0200

    Add main script to initialize AutoLoginManager and run automation flow.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Automating_JobApplications/main.py
@@ -0,0 +1,11 @@
+from Auto_Login_Manager import AutoLoginManager
+
+def main():
+    login_manager = AutoLoginManager()
+    login_manager.login_manager()
+    login_manager.auto_apply()
+
+
+
+if __name__ == "__main__":
+    main()
