21e5b61b4956e664a392a8db13169621b529a33f
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-06-04 22:38:27 +0200

    Main script to run ZillowScraper and print all links, addresses and prices.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- WebScraping_and_DataEntryCapstone/main.py
+++ WebScraping_and_DataEntryCapstone/main.py
@@ -2,6 +2,9 @@ from Zillow_Scraper import ZillowScraper
 
 def main():
     scraper = ZillowScraper("https://appbrewery.github.io/Zillow-Clone/")
+    scraper.get_links()
+    scraper.get_addresses()
+    scraper.get_prices()
 
 if __name__ == "__main__":
     main()
