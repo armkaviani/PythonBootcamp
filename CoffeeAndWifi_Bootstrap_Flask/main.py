73cdb71f994d29a7003261a169555f41bd3109c6
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-01 21:43:07 +0200

    Add route to read cafe data from CSV and render it in cafes template.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- CoffeeAndWifi_Bootstrap_Flask/main.py
+++ CoffeeAndWifi_Bootstrap_Flask/main.py
@@ -29,5 +29,15 @@ def add_cafe():
         return redirect(url_for('cafes'))    
     return render_template('add.html', form=form)
 
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
 if __name__ == '__main__':
     app.run(debug=True)
