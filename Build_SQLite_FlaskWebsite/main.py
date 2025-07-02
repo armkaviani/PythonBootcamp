204c9cb8025011f564f1de77771f90c6ba32b25d
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-02 21:40:42 +0200

    Show list of books in homepage.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Build_SQLite_FlaskWebsite/main.py
+++ Build_SQLite_FlaskWebsite/main.py
@@ -1,18 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 
 all_books = []
@@ -20,7 +7,7 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', bookss=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
