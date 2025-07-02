e1a3de4aeba6464e0a2447c1f6ab2f6c6dce3027
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-02 20:42:07 +0200

    The book details have been added as a dictionary.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Build_SQLite_FlaskWebsite/main.py
+++ Build_SQLite_FlaskWebsite/main.py
@@ -20,12 +20,21 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html')
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    if request.method== "POST":
+        book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(book)
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
