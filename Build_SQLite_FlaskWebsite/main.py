b0a32e83f67f0f8303dac19f0867bfa457066a33
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-04 23:06:08 +0200

    Initialize SQLAlchemy with SQLite database and custom declarative base.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Build_SQLite_FlaskWebsite/main.py
+++ Build_SQLite_FlaskWebsite/main.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from base import Base
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+
 
 all_books = []
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', bookss=all_books)
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add", methods=['GET', 'POST'])
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method== "POST":
+    if request.method == "POST":
         book = {
             "title": request.form["title"],
             "author": request.form["author"],
