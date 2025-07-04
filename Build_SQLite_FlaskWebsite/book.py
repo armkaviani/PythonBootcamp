c8fa4a90297584af54d6c91dca49d5597fdf054c
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-04 23:10:48 +0200

    Create Book table with id, title, author, and rating columns.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Build_SQLite_FlaskWebsite/book.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from main import db
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
