4b2629f37636555da97352149c2e1c6d4d9e05d6
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-08 22:28:59 +0200

    Add Movie model with fields for title, year, description, rating, ranking, review, and image URL.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Top_Movies_Website/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from base import db
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
