74e65da63acf0a8094abc8c2fca1f6e7af2681aa
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-09 22:29:35 +0200

    Add /add route to search movies via API and display selection options.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Top_Movies_Website/main.py
+++ Top_Movies_Website/main.py
@@ -2,14 +2,27 @@ from flask import Flask, render_template, redirect, url_for, request
 import requests
 from base import app, db
 from model import Movie
-from form import RateMovieForm
+from form import RateMovieForm, FindMovieForm
 
-    
+MOVIE_DB_API_KEY = ""
+MOVIE_DB_SEARCH_URL = ""
+
 @app.route("/")
 def home():
     result = db.session.execute(db.select(Movie))
     all_movies = result.scalars().all()
     return render_template("index.html", movies=all_movies)
+
+@app.route('/add', methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    
+    render_template("add.html", form=form)
 
 @app.route("/edit", methods=["GET", "POST"])
 def rate_movie():
