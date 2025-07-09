3090611d9e4ffed6528aadac362758bab95309b2
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-09 23:00:33 +0200

    Add /find route to fetch movie details from TMDb and save to database.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Top_Movies_Website/main.py
+++ Top_Movies_Website/main.py
@@ -13,6 +13,7 @@ def home():
     all_movies = result.scalars().all()
     return render_template("index.html", movies=all_movies)
 
+
 @app.route('/add', methods=["GET", "POST"])
 def add_movie():
     form = FindMovieForm()
@@ -23,6 +24,25 @@ def add_movie():
         return render_template("select.html", options=data)
     
     render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVI_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data =response.json()
+        new_movie = Movie(
+            title=data["title"], 
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description = data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("home"))
+    
 
 @app.route("/edit", methods=["GET", "POST"])
 def rate_movie():
@@ -35,6 +55,7 @@ def rate_movie():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
+
 
 @app.route('/delete')
 def delete_movie():
