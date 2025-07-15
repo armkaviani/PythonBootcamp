35b2954a0806eec58262b9d7ef9af4770d548476
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-15 22:23:16 +0200

    Alternatively how to convert dic to json.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Building_API_RESTful/main.py
+++ Building_API_RESTful/main.py
@@ -22,21 +22,7 @@ def get_random_cafe():
     result = db.session.execute(db.select(Cafe))
     all_cafes = result.scalars().all()
     random_cafe = random.choice(all_cafes)
-    return jsonify(cafe={
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        
-        "amenities": {
-          "seats": random_cafe.seats,
-          "has_toilet": random_cafe.has_toilet,
-          "has_wifi": random_cafe.has_wifi,
-          "has_sockets": random_cafe.has_sockets,
-          "can_take_calls": random_cafe.can_take_calls,
-          "coffee_price": random_cafe.coffee_price,
-        }
-    })
+    return jsonify(cafe=random_cafe.to_dict())
 
     
 
