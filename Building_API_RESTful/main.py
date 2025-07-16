62de430e4afd5c383970bec9a4b075bd250d700a
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-16 19:58:25 +0200

    Delete a cafe with a particular id. Cheange the request type to "Delete" in Postman.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- Building_API_RESTful/main.py
+++ Building_API_RESTful/main.py
@@ -91,13 +91,31 @@ def update_price(cafe_id):
         return jsonify(error= {"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
   
 
-
-
-
-
-
-
 # HTTP DELETE - Delete Record
+
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api_key")
+    if api_key == "TopSecretAPIKey":
+        try:
+            cafe = db.session.get(Cafe, cafe_id)
+
+            if cafe is None:
+                raise AttributeError("Cafe not found")
+
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+            
+        except AttributeError:
+            return jsonify(error= {"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+        
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct API key."}), 403
+        
+
+
+
 
 
 if __name__ == '__main__':
