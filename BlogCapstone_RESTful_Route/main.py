f6d6bd1e1d483d9dfe98303fbc8aa3efa3fbb8a2
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-17 22:16:22 +0200

    Get Blog Post Item.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ BlogCapstone_RESTful_Route/main.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+from base import app, db
+from model import BlogPost
+
+'''
+Make sure the required packages are installed: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from the requirements.txt for this project.
+'''
+
+
+@app.route('/')
+def get_all_posts():
+    # TODO: Query the database for all the posts. Convert the data to a python list.
+    all_posts = db.session.execute(db.select(BlogPost))
+    posts = all_posts.scalars().all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # TODO: Retrieve a BlogPost from the database based on the post_id
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+# Below is the code from previous lessons. No changes needed.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
