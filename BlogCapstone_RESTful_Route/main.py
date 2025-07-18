957e77a0293891c7d6c3c0ba8ba31aa020e37583
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-18 11:58:31 +0200

    The delete method has been implemented to delete the posts.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- BlogCapstone_RESTful_Route/main.py
+++ BlogCapstone_RESTful_Route/main.py
@@ -80,11 +80,14 @@ def edit_post(post_id):
 
 
 
-
-
-
-
 # TODO: delete_post() to remove a blog post from the database
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
