a9ebf4dee59b6901525fb7a2568b40c296dc04b0
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-07-22 21:43:53 +0200

    Add main Flask routes and initialize app structure.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ Authentication_Flask/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, current_user, logout_user
+from base import app, db, login_manager
+from model import User
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+def secrets():
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/download')
+def download():
+    pass
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
