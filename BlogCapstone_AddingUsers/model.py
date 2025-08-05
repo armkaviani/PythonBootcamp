d2aff7a13caa46f0044ca085161e6bdaf08b51f6
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-05 22:04:16 +0200

    The Blog post table has been configured.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ BlogCapstone_AddingUsers/model.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from base import db
+
+# CONFIGURE TABLES
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+# TODO: Create a User table for all your registered users. 
