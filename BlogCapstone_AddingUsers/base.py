97b6d15da62ebe110bd4e6414872b38320c2b731
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-05 22:15:06 +0200

    Initial setup with Flask, Bootstrap, CKEditor, and SQLAlchemy.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ BlogCapstone_AddingUsers/base.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+# TODO: Configure Flask-Login
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
