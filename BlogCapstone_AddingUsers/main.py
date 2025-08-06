7cfffddc5cb45fd06bfbe8dc051f505a7d26c7d8
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-06 22:28:21 +0200

    authenticate the user with Flask-Login.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- BlogCapstone_AddingUsers/main.py
+++ BlogCapstone_AddingUsers/main.py
@@ -43,6 +43,8 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+
+        login_user(new_user)
         return redirect(url_for("get_all_posts"))
     return render_template("register.html", form=form)
 
