9341ad160c2128d3cd5f2a1425f7a267e4c1ee95
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-06 20:26:04 +0200

    Users table for all registered users has been created.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- BlogCapstone_AddingUsers/model.py
+++ BlogCapstone_AddingUsers/model.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from base import db
+from flask_login import UserMixin
 
 # CONFIGURE TABLES
 class BlogPost(db.Model):
@@ -15,3 +16,9 @@ class BlogPost(db.Model):
 
 
 # TODO: Create a User table for all your registered users. 
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(250), unique=False)
+    password: Mapped[str] = mapped_column(String(250))
+    name: Mapped[str] = mapped_column(String(100))
