569fc5d7d559a3a08cfd557d8d4907b43ca9350e
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-07 22:59:30 +0200

    Added relationship from User to BlogPost using back_populates.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- BlogCapstone_AddingUsers/model.py
+++ BlogCapstone_AddingUsers/model.py
@@ -3,17 +3,6 @@ from sqlalchemy import Integer, String, Text
 from base import db
 from flask_login import UserMixin
 
-# CONFIGURE TABLES
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
-    date: Mapped[str] = mapped_column(String(250), nullable=False)
-    body: Mapped[str] = mapped_column(Text, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
-
 
 # TODO: Create a User table for all your registered users. 
 class User(UserMixin, db.Model):
@@ -22,3 +11,15 @@ class User(UserMixin, db.Model):
     email: Mapped[str] = mapped_column(String(250), unique=False)
     password: Mapped[str] = mapped_column(String(250))
     name: Mapped[str] = mapped_column(String(100))
+
+    posts = relationship("BlogPost", back_populates="author")
+
+# CONFIGURE TABLES
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
