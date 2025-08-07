6e4fec94483bf00b4463cba0792a67b0a741316c
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-07 22:41:45 +0200

    The @admin_only decorated has been created and added to protect Routes.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- BlogCapstone_AddingUsers/main.py
+++ BlogCapstone_AddingUsers/main.py
@@ -99,8 +99,18 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post,  current_user=current_user)
 
 
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.id != 1:
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 # TODO: Use a decorator so only an admin user can create a new post
 @app.route("/new-post", methods=["GET", "POST"])
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -120,6 +130,7 @@ def add_new_post():
 
 # TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -142,6 +153,7 @@ def edit_post(post_id):
 
 # TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
