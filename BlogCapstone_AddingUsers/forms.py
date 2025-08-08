4412b7745ae9b93947697b46012611c0c8d1b0bb
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-08-08 10:51:19 +0200

    The comment form has been created.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- BlogCapstone_AddingUsers/forms.py
+++ BlogCapstone_AddingUsers/forms.py
@@ -27,5 +27,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Let Me In!")
 
 
-
 # TODO: Create a CommentForm so users can leave comments below posts
+class CommentForm(FlaskForm):
+    comment = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
